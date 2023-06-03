a35e0d07a75db62006e2143eb71f356b22ebb944
Durvesh27 <132134129+Durvesh27@users.noreply.github.com>
2023-06-03 21:00:06 +0530

    update

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // var target=25;
-// var array=[10,13,45,23,23,15];
-// for(i=0;i<array.length;i++){
-//     for(j=i+1;j<array.length;j++){
-//         // if(array[i]+array[j]==target)
-//         // console.log(array[i],array[j])
-//     console.log(array[i],array[j])
-
-//     }
+// var array=[10,13,45,23,25,15];
+// for(i=0;i<array.length-2;i++){
+//     for(j=i+1;j<array.length-1;j++){
+//      console.log(array[i],"array[i]",array[j],"array[j]")
+    
 // }
+
+
+
 // Question 1: Given an target find two number from array which subtration is target
 // var target = 4;
 // var array = [3, 54, 65, 32, 6, 23, 56, 76, 17, 100, 7];
@@ -23,19 +23,31 @@
 // }
 
 // Question 2:  Given an target find three number from array which additionation is target
-var target = 126;
+// var target = 126;
 
-var array = [3, 55, 65, 32, 6, 23, 56, 76, 17, 100, 7, 91, 27, 8];
-for (i = 0; i < array.length; i++) {
-    for (j = i + 1; j < array.length; j++) {
-        for (s = i + 2; s < array.length; s++) {
-            if (array[i] + array[j] + array[s] == target) {
-                console.log(array[i], array[j], array[s])
+// var array = [3, 55, 65, 32, 6, 23, 56, 76, 17, 100, 7, 91, 27, 8];
+// for (i = 0; i < array.length; i++) {
+//     for (j = i + 1; j < array.length; j++) {
+//         for (s = i + 2; s < array.length; s++) {
+//             if (array[i] + array[j] + array[s] == target) {
+//                 console.log(array[i], array[j], array[s])
+//             }
+//         }
+//     }
+
+// }
+var target = 25;
+var array = [ 2,3,5,5,32,54,23,12,45,23,43,1,34,65,1,54,6,33,4,9,7 ];
+for(i=0;i<array.length;i++){
+    for(j=i+1;j<array.length-1;j++){
+        for(k=j+1;k<array.length-2;k++){
+            for(l=k+1;l<array.length-3;l++){
+            if(array[i]+array[j]+array[k]+array[l]==target){
+                console.log(array[i],array[j],array[k],array[l],"got it...")
+            }
             }
         }
     }
-
 }
-
 
 
